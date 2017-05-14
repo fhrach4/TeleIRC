524ba388d36295dda53a0cc98dd5ca8294b0ac6a
Frank Hrach IV <fhrach4@fastmail.com>
2017-05-13 23:49:29 -0400

    Changed server settings to a service

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,32 +3,48 @@ angular.module('starter.controllers', [])
   .controller('AppCtrl', function ($scope) {
   })
 
-  .controller('ServerCtrl', function ($scope, $http, $state) {
+  .controller('ServerCtrl', function ($scope, $http, $state, serverSettings) {
+
+    serverSettings.all().then(function (data) {
+      console.log(data);
+      $scope.servers = data;
+    })
 
     // load JSON
-    $http.get('data/servers.json').success(
-      function (data) {
-        $scope.servers = data["servers"];
-      }
-    );
+    // $http.get('data/servers.json').success(
+    //   function (data) {
+    //     $scope.servers = data["servers"];
+    //   }
+    // );
   })
 
-  .controller('ServerSettingCtrl', function ($scope, $http, $state) {
+  .controller('ServerSettingCtrl', function ($scope, $http, $state, ircListener, serverSettings) {
     var server_id = $state.params.serverID;
+    var data = serverSettings.get(server_id);
+
+    serverSettings.all().then(function (data) {
+      console.log(data);
+      $scope.server_title = data[server_id].title;
+      $scope.timestamps = data[server_id].timestamps;
+      $scope.notifications = data[server_id].notifications;
+      $scope.highlights = data[server_id].highlights;
+      $scope.address = data[server_id].address;
+      $scope.nick = data[server_id].nick;
+      $scope.channels = data[server_id].channels;
+    })
 
     // load JSON
-    $http.get('data/servers.json').success(
-      function (data) {
-        $scope.server_title = data[server_id].title;
-        $scope.timestamps = data[server_id].timestamps;
-        $scope.notifications = data[server_id].notifications;
-        $scope.highlights = data[server_id].highlights;
-        $scope.address = data[server_id].address;
-        $scope.nick = data[server_id].nick;
-        $scope.channels = data[server_id].channels;
-      });
+    // $http.get('data/servers.json').success(
+    //   function (data) {
+    //     $scope.server_title = data[server_id].title;
+    //     $scope.timestamps = data[server_id].timestamps;
+    //     $scope.notifications = data[server_id].notifications;
+    //     $scope.highlights = data[server_id].highlights;
+    //     $scope.address = data[server_id].address;
+    //     $scope.nick = data[server_id].nick;
+    //     $scope.channels = data[server_id].channels;
+    //   });
   })
 
-  .controller('ChatCtrl', function ($scope, $http, $state) {
-
+  .controller('ChatCtrl', function ($scope, $http, $state, ircListener) {
   });
--- www/js/services.js
+++ www/js/services.js
@@ -1,50 +1,21 @@
 angular.module('starter.services', [])
 
-.factory('Chats', function() {
-  // Might use a resource here that returns a JSON array
+  .factory("ircListener", function () {
+    return {};
+  })
 
-  // Some fake testing data
-  var chats = [{
-    id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'img/ben.png'
-  }, {
-    id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'img/max.png'
-  }, {
-    id: 2,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'img/adam.jpg'
-  }, {
-    id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'img/perry.png'
-  }, {
-    id: 4,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
-    face: 'img/mike.png'
-  }];
-
-  return {
-    all: function() {
-      return chats;
-    },
-    remove: function(chat) {
-      chats.splice(chats.indexOf(chat), 1);
-    },
-    get: function(chatId) {
-      for (var i = 0; i < chats.length; i++) {
-        if (chats[i].id === parseInt(chatId)) {
-          return chats[i];
-        }
+  .factory("serverSettings", function ($q, $http) {
+    return {
+      all: function () {
+        return $http.get('data/servers.json').then(function (response) {
+          return response.data['servers'];
+        });
+      },
+      get: function (serverID) {
+        return $http.get('data/servers.json').then(function (response) {
+          console.log(serverID);
+          return response.data['servers'][serverID];
+        });
       }
-      return null;
     }
-  };
-});
+  });
